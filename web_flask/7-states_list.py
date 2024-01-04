bbf1c9d6bdfe8ec0cf8f21071be602b63535d0e6
Selomon1 <soya.vandy1950@gmail.com>
2024-01-04 19:07:22 +0300

    AirBnB clone Web Framework

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,9 +3,8 @@
 Script to start a Flask web application that displays a list of States
 """
 from flask import Flask, render_template
-from models import storage
 from models import *
-
+from models import storage
 app = Flask(__name__)
 
 
